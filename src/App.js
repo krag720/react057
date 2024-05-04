415ce9ac4883c7e65e43e29d3971e8a62bfc31e4
krag720 <krag720@gmail.com>
2024-05-04 19:05:59 +0300

    lab2
    В побочной ветке реализовал возможность добавлять свои загадки. Вроде должно работать

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-
 import './App.css';
-import Item from './components/Item';
+import Button from "./components/Button";
+import QuestionForm from "./components/Adding";
 
 function App() {
 
 
-  return (
-    <div className="App">
-     <Item/>
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>СОЗДАТЬ НОВУЮ ЗАГАДКУ</h1>
+            <QuestionForm/>
+            <br/>
+            <Button/>
+        </div>
+    );
 }
 
 export default App;
